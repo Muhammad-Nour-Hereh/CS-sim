0a94bef86583159823930f7a8baf51ee0a4210b5
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-04-26 17:01:03 +0300

    create api routes

--- server/app/Http/Controllers/AuthController.php
+++ server/app/Http/Controllers/AuthController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function me() {}
+    public function register() {}
+    public function login() {}
+    public function logout() {}
 }
---
+++ server/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(["prefix" => "v0.1"], function () {
+
+        Route::prefix('auth')->group(function () {
+            Route::post('/login', [AuthController::class, 'login']);
+            Route::post('/register', [AuthController::class, 'register']);
+
+            // Protected routes (require JWT token)
+            Route::middleware('jwt')->group(function () {
+                Route::get('/me', [AuthController::class, 'me']);
+                Route::post('/logout', [AuthController::class, 'logout']);
+            });
+        });
+});
