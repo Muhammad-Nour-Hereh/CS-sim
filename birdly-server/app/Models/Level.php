68d52ee4aa895e45e8613c6f93681bcf01248c73
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-04-30 22:04:37 +0300

    create level model

--- birdly-server/app/Models/Level.php
+++ birdly-server/app/Models/Level.php
@@ -5,8 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Level extends Model
-{
-    /** @use HasFactory<\Database\Factories\LevelFactory> */
+class Level extends Model {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'course_id',
+    ];
+
+    public function course() {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function questions() {
+        return $this->belongsToMany(Question::class);
+    }
 }
