23de77e3d52ae0815f4c21f22f4d0855ff51a581
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-01 20:46:52 +0300

    craete progress migration

--- birdly-server/database/migrations/2025_05_01_172715_create_progresses_table.php
+++ birdly-server/database/migrations/2025_05_01_172715_create_progresses_table.php
@@ -4,24 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+
+    public function up(): void {
         Schema::create('progress', function (Blueprint $table) {
             $table->id();
+            $table->integer('exp');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('progress');
     }
 };
