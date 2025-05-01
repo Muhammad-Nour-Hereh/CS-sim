6af998a49b61872158b865f141432d0efaf15438
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-01 11:36:28 +0300

    fix a bug

--- birdly-server/app/Http/Requests/QuestionRequest.php
+++ birdly-server/app/Http/Requests/QuestionRequest.php
@@ -10,6 +10,7 @@ class QuestionRequest extends BaseFormRequest {
 
     public function rules(): array {
         return [
+            'course_id' => 'required|exists:courses,id',
             'title' => 'required|string|max:255',
             'question_type' => 'required|in:select,match,write,order',
             'content' => 'required|json',
