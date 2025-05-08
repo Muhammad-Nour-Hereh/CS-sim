078ce9798f88a6bb6db91c52392c46d8cbba281a
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-08 12:29:25 +0300

    add home page

--- birdly-client/src/pages/App.tsx
+++ birdly-client/src/pages/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { ROUTES } from "../lib/routes"
+import { HomePage } from "./HomePage"
 
 const App = () => {
 
@@ -7,7 +8,7 @@ const App = () => {
     <>
       <Routes>
         <Route path={ROUTES.ROOT} element={<Navigate to={ROUTES.HOME} replace />} />
-        <Route path={ROUTES.HOME} element={<h1>home</h1>} />
+        <Route path={ROUTES.HOME} element={<HomePage />} />
         <Route path={ROUTES.LOGIN} element={<h1>login</h1>} />
         <Route path={ROUTES.REGISTER} element={<h1>registration</h1>} />
         <Route path={ROUTES.QUIZ} element={<h1>quiz</h1>} />
