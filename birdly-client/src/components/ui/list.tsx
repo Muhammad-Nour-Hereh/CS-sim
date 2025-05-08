ecfa66e6832ac5e608a6db1c33af5eb46ca8a14e
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-09 02:29:48 +0300

    clean list

--- birdly-client/src/components/ui/list.tsx
+++ birdly-client/src/components/ui/list.tsx
@@ -14,19 +14,15 @@ const AnswerList = ({
           key={index}
           className={cn("flex items-stretch border-2 rounded-lg p-4 bg")}
         >
-          <div className="flex items-start">
-
-            {/* ring for number */}
-            <div
-              className={cn(
-                "flex items-center justify-center w-8 h-8 rounded-sm border-2 bg",
-
-              )}
-            >
-              <span className={cn("text-md font-bold text-border")}>{index + 1}</span>
-            </div>
+          {/* ring for number */}
+          <div
+            className={cn(
+              "flex items-center justify-center w-8 h-8 rounded-sm border-2 bg",
+            )}
+          >
+            <span className={cn("text-md font-bold text-border")}>{index + 1}</span>
           </div>
-
+          
           <div className={cn("ml-4 flex-1 self-center")}>{item}</div>
         </li>
       ))}
