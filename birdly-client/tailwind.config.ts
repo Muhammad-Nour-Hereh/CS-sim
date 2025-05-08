69628e123ab2184e28608ed297328d89ba891e79
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-08 23:54:11 +0300

    create global style

---
+++ birdly-client/tailwind.config.ts
@@ -0,0 +1,11 @@
+export const theme = {
+    extend: {
+        colors: {
+            primary: '#1e40af', // your custom primary color
+            'primary-foreground': '#ffffff',
+            secondary: '#f59e0b',
+            'secondary-foreground': '#000000',
+        },
+    },
+};
+export const plugins = [];
