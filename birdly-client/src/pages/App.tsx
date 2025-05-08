ef3850209088df5cc383f95812d7973be4557aaa
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-08 12:12:35 +0300

    create ROUTES constant

--- birdly-client/src/pages/App.tsx
+++ birdly-client/src/pages/App.tsx
@@ -1,13 +1,15 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { ROUTES } from "../lib/routes"
 
 const App = () => {
 
   return (
     <>
       <Routes>
-        <Route path="/home" element={<h1>home</h1>} />
-        <Route path="/login" element={<h1>login</h1>} />
-        <Route path="/registration" element={<h1>registration</h1>} />
+        <Route path={ROUTES.ROOT} element={<Navigate to={ROUTES.HOME} replace />} />
+        <Route path={ROUTES.HOME} element={<h1>home</h1>} />
+        <Route path={ROUTES.LOGIN} element={<h1>login</h1>} />
+        <Route path={ROUTES.REGISTER} element={<h1>registration</h1>} />
       </Routes>
 
     </>
