6476ced79d9354201b4e317a31a2b2ef2f090e44
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-08 18:00:04 +0300

    create button comonent

--- birdly-client/src/ui/pages/ComponentsPage.tsx
+++ birdly-client/src/ui/pages/ComponentsPage.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button"
 import MyComponent from "../components/MyComponent"
 
 const ComponentsPage = () => {
     return (
-        <div>
-            Components:
+        <div className="flex flex-col gap-2 bg-amber-200 p-4 center items-center">
             <MyComponent />
+            <Button>Button</Button>
+            <Button variant="outline">Button</Button>
+            <Button variant="destructive">Button</Button>
+            <Button variant="secondary">Button</Button>
+            <Button variant="ghost">Button</Button>
+            <Button variant="link">Button</Button>
         </div>
     )
 }
