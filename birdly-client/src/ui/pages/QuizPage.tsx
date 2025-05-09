308864162ea5d40a2e6f50fbea896ca562e7d2fd
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-09 13:06:28 +0300

    create quiz skeleton

--- birdly-client/src/ui/pages/QuizPage.tsx
+++ birdly-client/src/ui/pages/QuizPage.tsx
@@ -1,7 +1,23 @@
 const QuizPage = () => {
-  return (
-    <div>QuizPage</div>
-  )
+    return (
+        <div className="h-screen w-screen flex flex-col bg-[#0d1117] text-white p-4 ">
+            {/* Top: Progress Bar */}
+            <header className="h-12 flex items-center justify-between  bg-gray-400">
+                header
+            </header>
+
+            {/* Middle: Question and Answers */}
+            <main className="flex-1 flex flex-col justify-center items-center space-y-6  bg-gray-900">
+                quiz content
+            </main>
+
+            {/* Bottom: Buttons */}
+            <footer className="h-16 flex justify-between items-center bg-gray-400">
+                buttom
+            </footer>
+        </div>
+    )
 }
+
 
 export default QuizPage
