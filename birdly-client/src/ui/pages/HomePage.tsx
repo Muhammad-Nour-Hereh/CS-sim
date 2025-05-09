90e4bf284b828168d952e59f711830b7b31514cb
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-09 12:51:33 +0300

    create home page skeleton

--- birdly-client/src/ui/pages/HomePage.tsx
+++ birdly-client/src/ui/pages/HomePage.tsx
@@ -1,5 +1,17 @@
 export const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className="flex w-screen h-screen">
+        <aside className="w-[240px] bg-gray-800 text-white">
+          sidebar
+        </aside>
+  
+        <main className="flex-1 flex flex-col items-center bg-white">
+          map
+        </main>
+  
+        <section className="w-[240px] bg-gray-200">
+          right section
+        </section>
+      </div>
     )
 }
