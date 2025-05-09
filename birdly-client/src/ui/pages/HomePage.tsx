b0f203755886d3d5055374bee8883cbfaeaba07b
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-09 12:56:57 +0300

    create home page sidebar

--- birdly-client/src/ui/pages/HomePage.tsx
+++ birdly-client/src/ui/pages/HomePage.tsx
@@ -1,17 +1,20 @@
+import Sidebar from "../components/Sidebar"
+
+
 export const HomePage = () => {
     return (
         <div className="flex w-screen h-screen">
-        <aside className="w-[240px] bg-gray-800 text-white">
-          sidebar
-        </aside>
-  
-        <main className="flex-1 flex flex-col items-center bg-white">
-          map
-        </main>
-  
-        <section className="w-[240px] bg-gray-200">
-          right section
-        </section>
-      </div>
+            <aside className="w-56 bg-gray-900 text-white">
+                <Sidebar />
+            </aside>
+
+            <main className="flex-1 flex flex-col items-center bg-blue-400">
+                map
+            </main>
+
+            <section className="w-[240px] bg-gray-200">
+                right section
+            </section>
+        </div>
     )
 }
