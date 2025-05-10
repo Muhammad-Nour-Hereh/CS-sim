27a626f3c51b5e33a1d1d9cc6b3b7e4ead3d8cb2
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-10 17:28:42 +0300

    create playground page states

--- birdly-client/src/ui/pages/Playground.tsx
+++ birdly-client/src/ui/pages/Playground.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
+
 const Playground = () => {
+  const [code, SetCode] = useState('code editor:')
+  const [output, SetOutput] = useState('output:')
+  const [feedback, SetFeedback] = useState('feedback:')
+
   return (
     <div className="flex h-screen w-screen flex-col gap-6 p-6">
-      <section className="flex w-full flex-1 rounded-2xl bg-[#273B42] p-4">
-        code editor:
+      <section className="flex w-full flex-1 flex-col rounded-2xl bg-[#273B42] p-4">
+        <p>{code}</p>
       </section>
-      <section className="flex w-full flex-1 rounded-2xl bg-[#273B42] p-4">
-        output:
+      <section className="flex w-full flex-1 flex-col rounded-2xl bg-[#273B42] p-4">
+        <span>output:</span>
+        <p>{output}</p>
       </section>
-      <section className="flex w-full flex-1 rounded-2xl bg-[#273B42] p-4">
-        ai output:
+      <section className="flex w-full flex-1 flex-col rounded-2xl bg-[#273B42] p-4">
+        <span>birdly feedback:</span>
+        <p>{feedback}</p>
       </section>
     </div>
   )
