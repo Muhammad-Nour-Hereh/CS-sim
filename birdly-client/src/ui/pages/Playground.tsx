d6dd138e82bae73839e1bec804a554cdaef265ec
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-10 17:08:05 +0300

    create playground page structure

--- birdly-client/src/ui/pages/Playground.tsx
+++ birdly-client/src/ui/pages/Playground.tsx
@@ -1,5 +1,17 @@
 const Playground = () => {
-  return <div>Playground</div>
+  return (
+    <div className="flex h-screen w-screen flex-col gap-6 p-6">
+      <section className="flex w-full flex-1 rounded-2xl bg-blue-400 p-4">
+        code editor:
+      </section>
+      <section className="flex w-full flex-1 rounded-2xl bg-blue-400 p-4">
+        output:
+      </section>
+      <section className="flex w-full flex-1 rounded-2xl bg-blue-400 p-4">
+        ai output:
+      </section>
+    </div>
+  )
 }
 
 export default Playground
