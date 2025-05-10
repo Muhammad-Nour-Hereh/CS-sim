3b6271f9b96c86daa1c265a8443794ee9f622475
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-10 18:03:08 +0300

    add code highlighter overflow

--- birdly-client/src/ui/pages/Playground.tsx
+++ birdly-client/src/ui/pages/Playground.tsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const Playground = () => {
-  const [code, SetCode] = useState('print("hello, world!")')
+  const [code, SetCode] = useState('print("hello, world!")\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
   const [output, SetOutput] = useState('hello, world!')
   const [feedback, SetFeedback] = useState('your code is awesome')
 
   return (
     <div className="flex h-screen w-screen flex-col gap-6 p-6">
-      <section className="flex w-full flex-1 flex-col rounded-2xl bg-[#273B42] p-4">
-        <SyntaxHighlighter language="python" style={oneDark}>
+      <section className="flex w-full flex-2 flex-col rounded-2xl bg-[#273B42] p-4 overflow-hidden">
+        <SyntaxHighlighter
+          language="python"
+          style={oneDark}
+          showLineNumbers={true}
+          wrapLines={true}>
+            
           {code}
         </SyntaxHighlighter>
       </section>
