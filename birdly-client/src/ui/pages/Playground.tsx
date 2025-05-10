4e12317044f626814faf04631db73421c28a8bb0
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-10 19:21:17 +0300

    implement code mirror

--- birdly-client/src/ui/pages/Playground.tsx
+++ birdly-client/src/ui/pages/Playground.tsx
@@ -1,8 +1,36 @@
 import { useState } from 'react'
 import { CodeEditor } from '../components/CodeEditor'
 import CodeMirror from '@uiw/react-codemirror'
-import { javascript } from '@codemirror/lang-javascript'
 import { python } from '@codemirror/lang-python'
+import { EditorView } from 'codemirror'
+
+export const duolingoDarkTheme = EditorView.theme({
+  '&': {
+    backgroundColor: 'transparent !important',
+    color: '#e0f2f1',
+    borderRadius: '12px',
+    padding: '8px',
+  },
+  '.cm-scroller': {
+    fontFamily: 'nunito, monospace',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    lineHeight: '1.6',
+    overflow: 'auto',
+  },
+  '.cm-gutters': {
+    backgroundColor: 'transparent !important',
+    color: '#8ca0a0',
+    border: 'none',
+  },
+  '.cm-lineNumbers .cm-gutterElement': {
+    paddingLeft: '4px',
+    paddingRight: '4px',
+  },
+  '.cm-activeLine': {
+    // backgroundColor: '#37415144', // subtle active line
+  },
+})
 
 const Playground = () => {
   const code =
@@ -12,13 +40,22 @@ const Playground = () => {
 
   return (
     <div className="flex h-screen w-screen flex-col gap-6 p-6">
-      <section className="flex w-full flex-2 flex-col overflow-hidden rounded-2xl bg-[#273B42] p-4">
-        <CodeMirror
-          value="const hello = 'world';"
-          height="200px"
-          extensions={[python()]}
-          onChange={(value) => console.log(value)}
-        />
+      <section className="flex w-full flex-4 flex-col overflow-hidden rounded-2xl bg-[#273B42]">
+
+          <CodeMirror
+            value={code}
+            height="100%"
+            width="100%"
+            extensions={[python(), duolingoDarkTheme]}
+            //   onChange={(value) => onChange(value)}
+            theme={duolingoDarkTheme}
+            basicSetup={{
+              lineNumbers: true,
+              highlightActiveLine: true,
+              autocompletion: false
+            }}
+          />
+
       </section>
       <section className="flex w-full flex-1 flex-col rounded-2xl bg-[#273B42] p-4">
         <span className="font-semibold">output:</span>
