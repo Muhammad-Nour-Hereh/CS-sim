fc8589689fd30378ae0c216a7c210e57b21ddf1c
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-10 17:12:26 +0300

    set playground page colors

--- birdly-client/src/ui/pages/Playground.tsx
+++ birdly-client/src/ui/pages/Playground.tsx
@@ -1,13 +1,13 @@
 const Playground = () => {
   return (
     <div className="flex h-screen w-screen flex-col gap-6 p-6">
-      <section className="flex w-full flex-1 rounded-2xl bg-blue-400 p-4">
+      <section className="flex w-full flex-1 rounded-2xl bg-[#273B42] p-4">
         code editor:
       </section>
-      <section className="flex w-full flex-1 rounded-2xl bg-blue-400 p-4">
+      <section className="flex w-full flex-1 rounded-2xl bg-[#273B42] p-4">
         output:
       </section>
-      <section className="flex w-full flex-1 rounded-2xl bg-blue-400 p-4">
+      <section className="flex w-full flex-1 rounded-2xl bg-[#273B42] p-4">
         ai output:
       </section>
     </div>
