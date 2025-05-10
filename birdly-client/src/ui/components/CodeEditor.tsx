ad60008fbe95296afac68f76e7c91a91f155ea59
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-10 19:38:19 +0300

    recreate CodeEditor

--- birdly-client/src/ui/components/CodeEditor.tsx
+++ birdly-client/src/ui/components/CodeEditor.tsx
@@ -1,21 +1,55 @@
-import { useState } from 'react'
-import { Prism } from 'react-syntax-highlighter'
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import CodeMirror from '@uiw/react-codemirror'
+import { python } from '@codemirror/lang-python'
+import { EditorView } from 'codemirror'
+
+export const myTheme = EditorView.theme({
+  '&': {
+    backgroundColor: 'transparent !important',
+    color: '#e0f2f1',
+    borderRadius: '12px',
+    padding: '8px',
+  },
+  '.cm-scroller': {
+    fontFamily: 'nunito, monospace',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    lineHeight: '1.6',
+    overflow: 'auto',
+  },
+  '.cm-gutters': {
+    backgroundColor: 'transparent !important',
+    color: '#8ca0a0',
+    // border: 'green 4px solid',
+  },
+  '.cm-lineNumbers .cm-gutterElement': {
+    paddingLeft: '4px',
+    paddingRight: '4px',
+  },
+  '.cm-activeLine': {
+    // backgroundColor: '#37415144', // subtle active line
+  },
+})
 
 interface CodeEditorProps {
   code: string
+  setCode: Function
   language?: string
 }
 
-export function CodeEditor({ code, language = 'python' }: CodeEditorProps) {
-    const [_code, SetCode] = useState(code)
+export function CodeEditor({ code, setCode }: CodeEditorProps) {
   return (
-    <Prism
-      language={language}
-      style={oneDark}
-      showLineNumbers={true}
-      wrapLines={true}>
-      {_code}
-    </Prism>
+    <CodeMirror
+      value={code}
+      height="100%"
+      width="100%"
+      extensions={[python(), myTheme]}
+      onChange={(value) => setCode(value)}
+      theme={myTheme}
+      basicSetup={{
+        lineNumbers: true,
+        highlightActiveLine: true,
+        autocompletion: false,
+      }}
+    />
   )
 }
--- birdly-client/src/ui/pages/Playground.tsx
+++ birdly-client/src/ui/pages/Playground.tsx
@@ -1,61 +1,18 @@
 import { useState } from 'react'
 import { CodeEditor } from '../components/CodeEditor'
-import CodeMirror from '@uiw/react-codemirror'
-import { python } from '@codemirror/lang-python'
-import { EditorView } from 'codemirror'
-
-export const duolingoDarkTheme = EditorView.theme({
-  '&': {
-    backgroundColor: 'transparent !important',
-    color: '#e0f2f1',
-    borderRadius: '12px',
-    padding: '8px',
-  },
-  '.cm-scroller': {
-    fontFamily: 'nunito, monospace',
-    fontSize: '16px',
-    fontWeight: 'bold',
-    lineHeight: '1.6',
-    overflow: 'auto',
-  },
-  '.cm-gutters': {
-    backgroundColor: 'transparent !important',
-    color: '#8ca0a0',
-    border: 'none',
-  },
-  '.cm-lineNumbers .cm-gutterElement': {
-    paddingLeft: '4px',
-    paddingRight: '4px',
-  },
-  '.cm-activeLine': {
-    // backgroundColor: '#37415144', // subtle active line
-  },
-})
 
 const Playground = () => {
-  const code =
-    'print("hello, world!")\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+  const [code, setCode] = useState(
+    'print("hello, world!")\n\n\n\n\n\n\n\n\n\n\n\n\n',
+  )
+
   const [output, SetOutput] = useState('hello, world!')
   const [feedback, SetFeedback] = useState('your code is awesome')
 
   return (
     <div className="flex h-screen w-screen flex-col gap-6 p-6">
       <section className="flex w-full flex-4 flex-col overflow-hidden rounded-2xl bg-[#273B42]">
-
-          <CodeMirror
-            value={code}
-            height="100%"
-            width="100%"
-            extensions={[python(), duolingoDarkTheme]}
-            //   onChange={(value) => onChange(value)}
-            theme={duolingoDarkTheme}
-            basicSetup={{
-              lineNumbers: true,
-              highlightActiveLine: true,
-              autocompletion: false
-            }}
-          />
-
+        <CodeEditor code={code} setCode={setCode} />
       </section>
       <section className="flex w-full flex-1 flex-col rounded-2xl bg-[#273B42] p-4">
         <span className="font-semibold">output:</span>
