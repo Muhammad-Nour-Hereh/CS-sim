c50bcc1e6572d1c86eb2e902eac02a9e63a179b9
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-10 08:02:08 +0300

    install prettier for tailwind classes sorting

--- birdly-client/src/ui/pages/QuizPage.tsx
+++ birdly-client/src/ui/pages/QuizPage.tsx
@@ -1,32 +1,35 @@
-import { Settings, X } from "lucide-react"
-import { Button } from "../components/Button"
-import { Progress } from "../components/Progress"
-import Quiz from "../components/Quiz"
-import IconButton from "../components/IconButton"
+import { Settings, X } from 'lucide-react'
+import { Button } from '../components/Button'
+import { Progress } from '../components/Progress'
+import Quiz from '../components/Quiz'
+import IconButton from '../components/IconButton'
 
 const QuizPage = () => {
-    return (
-        <div className="h-screen w-screen flex flex-col bg-[#0d1117] text-white">
-            {/* Top */}
-            <header className="h-12 flex items-center justify-center pt-10 gap-6">
-                <IconButton><X /></IconButton>
-                <IconButton><Settings /></IconButton>
-                <Progress value={30} />
-            </header>
+  return (
+    <div className="flex h-screen w-screen flex-col bg-[#0d1117]">
+      {/* Top */}
+      <header className="flex h-12 items-center justify-center gap-6 pt-10">
+        <IconButton>
+          <X />
+        </IconButton>
+        <IconButton>
+          <Settings />
+        </IconButton>
+        <Progress value={30} />
+      </header>
 
-            {/* Middle */}
-            <main className="flex-1 flex flex-col justify-center items-center space-y-6 bg-blue-400">
-                <Quiz title="question 1" content="abc" type="select" />
-            </main>
+      {/* Middle */}
+      <main className="flex flex-1 flex-col items-center justify-center space-y-6 bg-blue-400">
+        <Quiz title="question 1" content="abc" type="select" />
+      </main>
 
-            {/* Bottom */}
-            <footer className="h-33 flex justify-evenly items-center border-t-2">
-                <Button variant="ghost">Skip</Button>
-                <Button >Check</Button>
-            </footer>
-        </div>
-    )
+      {/* Bottom */}
+      <footer className="flex h-33 items-center justify-evenly border-t-2">
+        <Button variant="ghost">Skip</Button>
+        <Button>Check</Button>
+      </footer>
+    </div>
+  )
 }
-
 
 export default QuizPage
