1c0309a26a8891389c8945e79d2289b741c2d1de
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-11 23:05:16 +0300

    create register page structure

--- birdly-client/src/ui/pages/RegisterPage.tsx
+++ birdly-client/src/ui/pages/RegisterPage.tsx
@@ -1,5 +1,77 @@
+import { Separator } from '@radix-ui/react-separator'
+import { Button } from '../components/Button'
+import { Input } from '../components/Input'
+import useLoginPage from '@/hooks/useLoginPage'
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>
+  const {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    errors,
+    LoginHandle,
+    navigateRegisterHandle,
+    navigateForgetPasswordHandle,
+  } = useLoginPage()
+
+  return (
+    <div className="pg-background flex h-screen w-screen items-center justify-center">
+      <div className="bg-border flex w-150 flex-col items-center justify-center gap-4 rounded-2xl border-2 border-white p-9">
+        <h1 className="self-start text-3xl font-bold">Login</h1>
+        <Input
+          value={email}
+          setter={setEmail}
+          errorMsg={errors.email}
+          placeholder="name"
+          className="h-13 border-2 text-2xl font-bold"
+        />
+        <Input
+          value={email}
+          setter={setEmail}
+          errorMsg={errors.email}
+          placeholder="email"
+          className="h-13 border-2 text-2xl font-bold"
+        />
+        <Input
+          value={password}
+          setter={setPassword}
+          errorMsg={errors.password}
+          placeholder="password"
+          variant="password"
+          className="h-13 border-2 text-2xl font-bold"
+        />
+        <Input
+          value={email}
+          setter={setEmail}
+          errorMsg={errors.email}
+          placeholder="repeat password"
+          className="h-13 border-2 text-2xl font-bold"
+        />
+        <span
+          className="cursor-pointer self-start text-lg font-semibold"
+          onClick={navigateForgetPasswordHandle}>
+          forget password?
+        </span>
+        <Button
+          onClick={LoginHandle}
+          className="w-full text-xl font-bold text-white"
+          variant="borderless">
+          Register
+        </Button>
+        <Separator className="bg-white" />
+        <span className="self-start text-lg font-semibold">
+          already have an account!
+          <span
+            className="text-primary cursor-pointer font-semibold"
+            onClick={navigateRegisterHandle}>
+            {' '}
+            Login
+          </span>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default RegisterPage
