f7c76651ee7734f398ac0a3887eb153688c54a29
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-11 22:00:56 +0300

    add states to useLoginPage

--- birdly-client/src/hooks/useLoginPage.tsx
+++ birdly-client/src/hooks/useLoginPage.tsx
@@ -1,10 +1,55 @@
-import { useEffect } from 'react'
+import { remote } from '@/remotes/remotes'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const useLoginPage = () => {
   useEffect(() => {
     console.log('login page is loaded')
   }, [])
-  return
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState({})
+  const navigate = useNavigate()
+
+  const validateForm = () => {
+    const newErrors = {}
+
+    // if (!email) newErrors.email = 'Email is required'
+    // if (!password) newErrors.password = 'Password is required'
+
+    setErrors(newErrors)
+    return Object.keys(newErrors).length === 0
+  }
+
+  const handleLogin = async () => {
+    if (!validateForm()) return
+
+    try {
+      const res = await remote.login(email, password)
+      if (!res.success) {
+        setErrors({ general: res.message || 'Invalid login credentials' })
+        return
+      }
+
+      if (!res.payload.access_token) return
+
+      localStorage.setItem('access_token', res.payload.access_token)
+
+      navigate('/home')
+    } catch (error) {
+      setErrors({ general: 'Login failed. Please try again.' })
+    }
+  }
+
+  return {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    errors,
+    handleLogin,
+  }
 }
 
 export default useLoginPage
--- birdly-client/src/ui/pages/LoginPage.tsx
+++ birdly-client/src/ui/pages/LoginPage.tsx
@@ -4,13 +4,16 @@ import { Input } from '../components/Input'
 import { Separator } from '../components/Separator'
 
 const LoginPage = () => {
-  useLoginPage()
-  
+  const { email, setEmail, password, setPassword, errors, handleLogin } =
+    useLoginPage()
+
   return (
     <div className="pg-background flex h-screen w-screen items-center justify-center">
       <div className="bg-border flex w-150 flex-col items-center justify-center gap-4 rounded-2xl border-2 border-white p-9">
         <h1 className="self-start text-3xl font-bold">Login</h1>
         <Input
+          value={email}
+          // onChange={setEmail}
           placeholder="name"
           errorMsg="invalid"
           className="h-13 border-2 text-2xl font-bold"
