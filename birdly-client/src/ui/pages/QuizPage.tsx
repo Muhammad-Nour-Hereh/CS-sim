80893021741f3be22116f2605707ad3e0300063b
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-12 09:25:31 +0300

    uncommint order quiz

--- birdly-client/src/ui/pages/QuizPage.tsx
+++ birdly-client/src/ui/pages/QuizPage.tsx
@@ -25,7 +25,7 @@ const QuizPage = () => {
 
       {/* Middle */}
       <main className="flex w-150 flex-1 flex-col items-center justify-around space-y-6">
-        <Quiz question={questions[3]} />
+        <Quiz question={questions[2]} />
       </main>
 
       {/* Bottom */}
