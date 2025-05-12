51f11bc9572bbe613850b2184e1cd1094f7d19b2
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-12 18:06:26 +0300

    create Contexts wrapper for avoid nesting contexts

--- birdly-client/src/main.tsx
+++ birdly-client/src/main.tsx
@@ -3,17 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import AuthProvider from './contexts/AuthContext.tsx'
-import QuizProvider from './contexts/QuizContext.tsx'
+import Contexts from './contexts/Contexts.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <QuizProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QuizProvider>
-    </AuthProvider>
+    <Contexts>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Contexts>
   </StrictMode>,
 )
