e64883b15b32e401e1f15bfda3374fd28f82ee57
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-12 11:27:12 +0300

    create MatchQuiz

--- birdly-client/src/ui/components/quiz-components/MatchQuiz.tsx
+++ birdly-client/src/ui/components/quiz-components/MatchQuiz.tsx
@@ -1,4 +1,5 @@
-import { MatchQuestion } from "@/interfaces/question"
+import { MatchQuestion } from '@/interfaces/question'
+import Card from '../Card'
 
 const MatchQuiz = ({ question }: { question: MatchQuestion }) => {
   // prettier-ignore
@@ -6,8 +7,22 @@ const MatchQuiz = ({ question }: { question: MatchQuestion }) => {
   console.log(pairs)
   return (
     <>
-      <h1>{title}</h1>
-      <div>MatchQuiz </div>
+      <h1 className="self-start text-2xl font-bold">{title}</h1>
+      <div className="flex w-150 gap-4 py-4">
+        <div className="flex flex-1 flex-col gap-4">
+          {' '}
+          <Card>1</Card>
+          <Card>2</Card>
+          <Card>3</Card>
+        </div>
+        <div className="flex flex-1 flex-col gap-4">
+          {' '}
+          <Card>a</Card>
+          <Card>b</Card>
+          <Card>c</Card>
+        </div>
+      </div>
+
       {/* <div>{content[0][0]} </div> */}
     </>
   )
