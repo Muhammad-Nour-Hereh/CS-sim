87d7515b10b1e7db9ea6ea622167d5f0b307c52c
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-13 18:09:43 +0300

    connect GuestRoutes to backend

--- birdly-client/src/protected-routes/GuestRoutes.tsx
+++ birdly-client/src/protected-routes/GuestRoutes.tsx
@@ -1,11 +1,30 @@
+import { remote } from '@/remotes/remotes'
+import { useEffect, useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
 const GuestRoutes = () => {
-    const isAutherized = true
+  const [loading, setLoading] = useState(true)
+  const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null)
 
-    return (
-        isAutherized ? <Outlet /> : <Navigate to='/home' />
-    )
+  useEffect(() => {
+    const checkAuth = async () => {
+      const res = await remote.me()
+      const success = res.success === 'true'
+
+      if (!success) {
+        localStorage.removeItem('access_token')
+      }
+
+      setIsAuthorized(success)
+      setLoading(false)
+    }
+
+    checkAuth()
+  }, [])
+
+  if (loading) return <div>Loading...</div>
+
+  return isAuthorized ? <Navigate to="/home" /> : <Outlet />
 }
 
 export default GuestRoutes
