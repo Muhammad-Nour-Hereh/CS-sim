286da4bf376c13b721ee79d73ba9e1f961d50730
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-13 11:54:21 +0300

    add section 2

--- birdly-client/src/ui/pages/HomePage.tsx
+++ birdly-client/src/ui/pages/HomePage.tsx
@@ -2,9 +2,10 @@ import useHomePage from '@/hooks/useHomePage'
 import Sidebar from '../components/Sidebar'
 import { useEffect, useRef, useState } from 'react'
 import { Circle } from '../components/Circle'
+import { Separator } from '../components/Separator'
 
 const MAP_WIDTH = 800 // Can be any size
-const MAP_HEIGHT = 1500
+const MAP_HEIGHT = 800
 const CIRCLE_SIZE = 64
 
 export const HomePage = () => {
@@ -38,9 +39,51 @@ export const HomePage = () => {
         <Sidebar />
       </aside>
 
-      <main className="bg flex flex-1 flex-col items-center overflow-hidden">
+      <main className="bg flex flex-1 flex-col items-center">
         <h1 className="bg-primary m-10 rounded-full p-10 px-30 py-2 text-center text-4xl font-bold">
           Section 1
+        </h1>
+        <div
+          className={`relative h-fit w-full max-w-[90vmin]`}
+          style={{
+            aspectRatio: `${MAP_WIDTH} / ${MAP_HEIGHT}`,
+          }}>
+          {/* SVG */}
+          <svg
+            ref={svgRef}
+            viewBox={`0 0 ${MAP_WIDTH} ${MAP_HEIGHT}`}
+            preserveAspectRatio="xMidYMid meet"
+            className="absolute top-0 left-0 z-0 h-full w-full">
+            <path
+              ref={pathRef}
+              d="M 400 50 C 300 150, 300 250, 400 350 S 500 500, 400 600"
+              stroke="#fff"
+              strokeDasharray="10,10"
+              strokeWidth="4"
+              fill="none"
+            />
+          </svg>
+          {/* Circles */}
+          {positions.map((pos, idx) => (
+            <Circle
+              key={idx}
+              style={{
+                position: 'absolute',
+                left: `calc(${(pos.x / MAP_WIDTH) * 100}% - ${CIRCLE_SIZE / 2}px)`,
+                top: `calc(${(pos.y / MAP_HEIGHT) * 100}% - ${CIRCLE_SIZE / 2}px)`,
+                width: `${CIRCLE_SIZE}px`,
+                height: `${CIRCLE_SIZE}px`,
+              }}
+              onClick={naivgateQuizHandle}>
+              {nodes[idx]}
+            </Circle>
+          ))}
+        </div>
+
+        <Separator className="bg-white p-0.5" />
+
+        <h1 className="bg-primary m-10 rounded-full p-10 px-30 py-2 text-center text-4xl font-bold">
+          Section 2
         </h1>
         <div
           className={`relative h-fit w-full max-w-[90vmin]`}
