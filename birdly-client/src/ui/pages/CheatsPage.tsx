62778b53a4e3186dec1fce6e9d201d4ae0e1610d
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-15 02:45:52 +0300

    create cheats page

---
+++ birdly-client/src/ui/pages/CheatsPage.tsx
@@ -0,0 +1,21 @@
+import { MDXProvider } from '@mdx-js/react'
+import MyMdxFile from '../../assets/hello_world.mdx'
+import Sidebar from '../components/Sidebar'
+
+const CheatsPage = () => {
+  return (
+    <div className="flex h-screen w-screen">
+      <aside className="w-56 bg-gray-900 text-white">
+        <Sidebar />
+      </aside>
+
+      <main className="bg flex flex-1 flex-col p-4">
+        <MDXProvider>
+          <MyMdxFile />
+        </MDXProvider>
+      </main>
+    </div>
+  )
+}
+
+export default CheatsPage
