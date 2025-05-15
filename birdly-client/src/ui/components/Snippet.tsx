3bfb1465e0b43f394b84b33a701b635fd8749ce3
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-16 01:47:40 +0300

    integrate run code in snippet component

--- birdly-client/src/ui/components/Snippet.tsx
+++ birdly-client/src/ui/components/Snippet.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { cn } from '@/lib/utils'
 import { CodeEditor } from './CodeEditor'
+import { remote } from '@/remotes/remotes'
 
 interface SnippetProps {
   title?: string
@@ -19,11 +20,18 @@ const Snippet = ({
   className,
 }: SnippetProps) => {
   const [code, setCode] = useState(initCode)
+  const [output, setOutput] = useState('')
 
+  const run = async () => {
+    const res = await remote.run(code)
+    setOutput(res.data?.output || '')
+  }
+  
   const handleRunCode = () => {
     if (onRun) {
       onRun(code)
     } else {
+      run()
       console.log('Code executed:\n', code)
     }
   }
@@ -51,6 +59,9 @@ const Snippet = ({
           className="rounded-md bg-[#8bc34a] px-3 py-1 text-sm font-medium text-black transition-colors hover:bg-[#7cb342]">
           Try it your self &gt;
         </button>
+      </div>
+      <div className="border-t border-gray-700/30 bg-[#1e2a31] p-2">
+        {output}
       </div>
     </div>
   )
