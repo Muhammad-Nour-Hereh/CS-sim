b07c4f1b3fc7c6a2422772621b4b501ec6a104d0
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-16 10:19:46 +0300

    replace : by = in env/'s

--- birdly-client/src/remotes/axios_defaults.ts
+++ birdly-client/src/remotes/axios_defaults.ts
@@ -1,7 +1,1 @@
-export let baseURL = ""
-
-export async function loadEnv() {
-  const res = await fetch("/env.json")
-  const data = await res.json()
-  baseURL = data.base_url
-}
+export let baseURL = import.meta.env.VITE_API_BASE_URL
