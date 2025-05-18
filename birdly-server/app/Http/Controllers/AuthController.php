7b2c046c8a5e6d9c3b099d14fedf2a3b087e258a
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-18 23:32:47 +0300

    inject AuthService into AuthController

--- birdly-server/app/Http/Controllers/AuthController.php
+++ birdly-server/app/Http/Controllers/AuthController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use App\Services\AuthService;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 
 class AuthController extends Controller {
+
+    public function __construct(protected AuthService $auth) {
+    }
+
     public function me() {
         $user = JWTAuth::user();
         return $this->successResponse($user);
