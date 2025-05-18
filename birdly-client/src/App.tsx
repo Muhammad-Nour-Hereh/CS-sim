80c3e797c0dfd649940399b3fc45bf9a52cd577c
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-18 22:44:14 +0300

    move Context and BrowserRouter components to App

--- birdly-client/src/App.tsx
+++ birdly-client/src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+import Contexts from './contexts/Contexts'
 import AppRoutes from './ui/components/AppRoutes'
 
 const App = () => {
   console.log(import.meta.env.VITE_TEST)
-  return <AppRoutes />
+  return (
+    <Contexts>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </Contexts>
+  )
 }
 
 export default App
--- birdly-client/src/main.tsx
+++ birdly-client/src/main.tsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import Contexts from './contexts/Contexts.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Contexts>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Contexts>
+    <App />
   </StrictMode>,
 )
