c5140fc33fdaeb9a1fef03c2ddabf1e6abc69b62
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-19 01:41:24 +0300

    add CheatRepo to RepoProvider

--- birdly-server/app/Providers/RepoProvider.php
+++ birdly-server/app/Providers/RepoProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\CheatRepo;
 use Illuminate\Support\ServiceProvider;
 
 class RepoProvider extends ServiceProvider {
 
     public function register(): void {
-        //
+        $this->app->singleton(CheatRepo::class, function () {
+            return new CheatRepo();
+        });
     }
 }
