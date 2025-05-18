8e77ae34b9a63d94c6a310038f223e1563dd065d
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-18 23:40:25 +0300

    add and implement register() in AuthController

--- birdly-server/app/Http/Controllers/AuthController.php
+++ birdly-server/app/Http/Controllers/AuthController.php
@@ -16,13 +16,12 @@ class AuthController extends Controller {
 
     public function me() {
         $user = $this->auth->user();
+        
         return $this->successResponse($user);
     }
 
     public function register(RegisterRequest $request) {
-        $user = User::create($request->validated());
-
-        $token = JWTAuth::fromUser($user);
+        $token = $this->auth->register($request->validated());
 
         return $this->successResponse($token, 201);
     }
--- birdly-server/app/Services/AuthService.php
+++ birdly-server/app/Services/AuthService.php
@@ -2,10 +2,18 @@
 
 namespace App\Services;
 
+use App\Models\User;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthService {
     public function user() {
         return JWTAuth::user();
+    }
+
+    public function register($credentials) {
+        $user = User::create($credentials);
+        $token = JWTAuth::fromUser($user);
+
+        return $token;
     }
 }
