9f18789a4e069d7d0fd44bdf159a5d8aebad0bc9
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-18 23:30:05 +0300

    clean service provider form unused code

--- birdly-server/app/Providers/AppServiceProvider.php
+++ birdly-server/app/Providers/AppServiceProvider.php
@@ -9,31 +9,22 @@ use App\Services\SnippetRunnerService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
-    /**
-     * Register any application services.
-     */
+
     public function register(): void {
-        $this->app->singleton(SnippetRunnerService::class, function ($app) {
+        $this->app->singleton(SnippetRunnerService::class, function () {
             return new SnippetRunnerService();
         });
 
-        $this->app->singleton(GuildbookFileService::class, function ($app) {
+        $this->app->singleton(GuildbookFileService::class, function () {
             return new GuildbookFileService();
         });
 
-        $this->app->singleton(CheatFileService::class, function ($app) {
+        $this->app->singleton(CheatFileService::class, function () {
             return new CheatFileService();
         });
-        
-        $this->app->singleton(OpenAIService::class, function ($app) {
+
+        $this->app->singleton(OpenAIService::class, function () {
             return new OpenAIService(config('services.openai.api_key'));
         });
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void {
-        //
     }
 }
