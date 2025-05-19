45d6853de4e1146c10f763897b51bb74da0f77c3
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-19 11:48:20 +0300

    create find method in guildbook repo

--- birdly-server/app/Repositories/GuildbookRepo.php
+++ birdly-server/app/Repositories/GuildbookRepo.php
@@ -2,9 +2,17 @@
 
 namespace App\Repositories;
 
+use App\Models\Guildbook;
+
 class GuildbookRepo {
+
     public function all() {
         return Guildbook::all();
     }
+
+    public function find(int $id) {
+        return Guildbook::find($id);
+    }
+
 
 }
