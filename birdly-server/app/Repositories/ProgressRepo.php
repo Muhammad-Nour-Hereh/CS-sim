4ca3da3eac584b497ce7dd1fc612f2676e6de451
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-19 15:44:09 +0300

    create addMistake method in ProgressRepo

--- birdly-server/app/Repositories/ProgressRepo.php
+++ birdly-server/app/Repositories/ProgressRepo.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\Progress;
+use App\Models\Question;
 
 class ProgressRepo {
 
@@ -13,6 +14,25 @@ class ProgressRepo {
     public function getMistakes(int $progressId) {
         $progress = $this->find($progressId);
         return $progress?->mistakes()->get();
+    }
+
+    public function addMistake(int $progressId, int $questionId): bool {
+        $progress = Progress::find($progressId);
+        $question = Question::find($questionId);
+
+        if (!$progress || !$question) return false;
+
+        $mistake = $progress->mistakes()->where('question_id', $questionId)->first();
+
+        if ($mistake) {
+            $progress->mistakes()->updateExistingPivot($questionId, [
+                'count' => $mistake->pivot->count + 1
+            ]);
+        } else {
+            $progress->mistakes()->attach($questionId);
+        }
+
+        return true;
     }
 
 }
