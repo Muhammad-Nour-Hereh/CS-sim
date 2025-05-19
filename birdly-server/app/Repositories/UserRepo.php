e6e38c9dc64e97cde815931b5375a926481995a6
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-19 16:35:53 +0300

    create subscribe method in UserRepo

--- birdly-server/app/Repositories/UserRepo.php
+++ birdly-server/app/Repositories/UserRepo.php
@@ -2,13 +2,22 @@
 
 namespace App\Repositories;
 
+use App\Models\Course;
+use App\Models\Progress;
+use App\Models\User;
+
 class UserRepo
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
+    public function subscribe(User $user, int $courseId) {
+        $course = Course::find($courseId);
+        if (!$course) return ['status' => 'not_found'];
+
+        if ($user->courses()->where('course_id', $courseId)->exists())
+            return ['status' => 'already_subscribed'];
+
+        $progress = Progress::create();
+        $user->courses()->attach($courseId, ['progress_id' => $progress->id]);
+
+        return ['status' => 'subscribed'];
     }
 }
