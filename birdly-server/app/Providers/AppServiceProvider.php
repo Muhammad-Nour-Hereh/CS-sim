6fe2e87f92c4a076a12b1a3571f89deaa0da5b6a
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-19 17:27:44 +0300

    add missing attribute in appServiceProvider

--- birdly-server/app/Providers/AppServiceProvider.php
+++ birdly-server/app/Providers/AppServiceProvider.php
@@ -12,10 +12,15 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider {
 
     public function register(): void {
-        $this->app->singleton(SnippetRunnerService::class, fn () => new SnippetRunnerService());
-        $this->app->singleton(GuildbookFileService::class, fn () => new GuildbookFileService());
-        $this->app->singleton(CheatFileService::class, fn () => new CheatFileService());
-        $this->app->singleton(OpenAIService::class, fn () => new OpenAIService(config('services.openai.api_key')));
-        $this->app->singleton(AuthService::class, fn () => new AuthService());
+        $this->app->singleton(SnippetRunnerService::class, fn() => new SnippetRunnerService());
+        $this->app->singleton(GuildbookFileService::class, fn() => new GuildbookFileService());
+        $this->app->singleton(CheatFileService::class, fn() => new CheatFileService());
+        $this->app->singleton(AuthService::class, fn() => new AuthService());
+        $this->app->singleton(OpenAIService::class, function ($app) {
+            return new OpenAIService(
+                $app->make(GuildbookFileService::class),
+                config('services.openai.api_key')
+            );
+        });
     }
 }
