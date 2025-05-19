4b6f288764e4d8b35dae3daf4759b4bdee6d4261
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-19 11:18:59 +0300

    bind CourseRepo in RepoProvider

--- birdly-server/app/Providers/RepoProvider.php
+++ birdly-server/app/Providers/RepoProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Repositories\CheatRepo;
+use App\Repositories\CourseRepo;
 use Illuminate\Support\ServiceProvider;
 
 class RepoProvider extends ServiceProvider {
@@ -11,5 +12,10 @@ class RepoProvider extends ServiceProvider {
         $this->app->singleton(CheatRepo::class, function () {
             return new CheatRepo();
         });
+
+        $this->app->singleton(CourseRepo::class, function () {
+            return new CourseRepo();
+        });
+
     }
 }
--- birdly-server/app/Repositories/CourseRepo.php
+++ birdly-server/app/Repositories/CourseRepo.php
@@ -2,13 +2,5 @@
 
 namespace App\Repositories;
 
-class CourseRepo
-{
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+class CourseRepo {
 }
