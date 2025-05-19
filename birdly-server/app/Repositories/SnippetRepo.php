4c29234eca79ed7ef7734991ab76c034822c9714
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-19 16:10:34 +0300

    use SnippetRepo create method in store

--- birdly-server/app/Repositories/SnippetRepo.php
+++ birdly-server/app/Repositories/SnippetRepo.php
@@ -2,9 +2,19 @@
 
 namespace App\Repositories;
 
+use App\Models\Snippet;
+use App\Models\User;
+
 class SnippetRepo {
-    
+
     public function all(User $user) {
         return $user->snippets;
+    }
+
+    public function create(User $user, array $data) {
+        return Snippet::create([
+            'user_id' => $user->id,
+            ...$data,
+        ]);
     }
 }
