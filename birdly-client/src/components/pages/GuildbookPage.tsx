a320b627e426ed1c87ce1e62eb505fa2b0342236
Muhammadnourhereh <acadamiclearner@gmail.com>
2025-05-21 09:38:08 +0300

    remove comments from guildbook page

--- birdly-client/src/components/pages/GuildbookPage.tsx
+++ birdly-client/src/components/pages/GuildbookPage.tsx
@@ -5,9 +5,6 @@ import ChatArea from '../ui/ChatArea'
 import { remote } from '@/remotes/remotes'
 import { useEffect, useState } from 'react'
 import MDXContent from '../../assets/python_variables.mdx'
-// import { compile } from '@mdx-js/mdx'
-// import * as runtime from 'react/jsx-runtime'
-// import React from 'react'
 import { baseURL } from '@/remotes/axios_defaults'
 
 const GuildbookPage = () => {
@@ -16,7 +13,7 @@ const GuildbookPage = () => {
   useEffect(() => {
     const updatePath = async () => {
       const res = await remote.guildbook.getById(1)
-      const _path = baseURL + '/' + res.data?.path
+      const _path = baseURL + '/' + res.data?.content
       if (_path) setPath(_path)
     }
     updatePath()
@@ -61,69 +58,3 @@ const GuildbookPage = () => {
 }
 
 export default GuildbookPage
-
-// function MDXRenderer({ url }: any) {
-//   const [Content, setContent] = useState(() => () => (
-//     <div>Loading MDX content...</div>
-//   ))
-//   const [error, setError] = useState(null)
-
-//   useEffect(() => {
-//     async function fetchAndRenderMDX() {
-//       try {
-//         // 1. Fetch the MDX file
-//         // const response = await remote.fetchFile(url)
-//         // console.log(response)
-//         // if (!response.ok) {
-//         //   throw new Error(`Failed to fetch MDX: ${response.statusText}`)
-//         // }
-
-//         // const mdxContent = await response.text()
-//         const mdxContent = '# hello'
-
-//         // 2. Compile the MDX to JSX
-//         const compiled: any = await compile(mdxContent, {
-//           outputFormat: 'function-body',
-//           development: false,
-//         })
-
-//         // 3. Evaluate the compiled JSX
-//         const scope = {
-//           ...runtime,
-//           Fragment: React.Fragment,
-//           useDynamicImport: () => () => Promise.resolve(),
-//           useMDXComponents: () => ({}),
-//         }
-
-//         const keys = Object.keys(scope)
-//         const values = Object.values(scope)
-//         const fn = new Function(...keys, compiled.value)
-//         const MDXModule = fn(...values)
-
-//         // 4. Set the rendered component to state
-//         setContent(() => MDXModule.default)
-//       } catch (err: any) {
-//         console.error('Error rendering MDX:', err)
-//         setError(err.message)
-//       }
-//     }
-
-//     fetchAndRenderMDX()
-//   }, [url])
-
-//   if (error) {
-//     return <div>Error loading content: {error}</div>
-//   }
-
-//   return (
-//     <MDXProvider
-//       components={{
-//         h1: (props) => <h1 style={{ color: 'blue' }} {...props} />,
-//         h2: (props) => <h2 style={{ color: 'green' }} {...props} />,
-//         p: (props) => <p style={{ lineHeight: 1.5 }} {...props} />,
-//         // Add more component overrides as needed
-//       }}>
-//       <Content />
-//     </MDXProvider>
-//   )
-// }
